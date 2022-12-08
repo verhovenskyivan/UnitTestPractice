b4043970c8cff845e5fac07a67f5bf6a8c0a6ac1
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-08 11:55:12 +0300

    Add files via upload

--- SolverTest.cs
+++ SolverTest.cs
@@ -5,6 +5,16 @@ namespace UnitTestPracticing
     [TestClass]
     public class SolverTest
     {
+
+        void TestEquation(double a, double b, double c, params double[] expectedResult)
+        {
+            var result = QuadraticEquationsSolver.Solve(a, b, c);
+
+            Assert.AreEqual(expectedResult.Length, result.Length);
+
+            for (int i = 0; i < result.Length; i++)
+                Assert.AreEqual(expectedResult[i], result[i]);
+        }
         [TestMethod]
         public void OrdinaryEquation()
         {
@@ -28,6 +38,40 @@ namespace UnitTestPracticing
             var result = QuadraticEquationsSolver.Solve(a, b, c);
 
             Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void ZeroDiscriminant()
+        {
+            TestEquation(1, 2, 1,-1);
+        }
+        [TestMethod]
+        public void NegativeDiscriminant()
+        {
+            TestEquation(1, 1, 1);
+        }
+        [TestMethod]
+        public void ZeroA()
+        {
+            TestEquation(0, -1, 1, 1);
+        }
+        [TestMethod]
+        public void ZeroAandB()
+        {
+            TestEquation(0, 0, 1);
+        }
+        [TestMethod]
+        public void FunctionalTest()
+        {
+            var rnd = new Random();
+            var a = rnd.NextDouble() * 10;
+            var b = rnd.NextDouble() * 10;
+            var c = rnd.NextDouble() * 10;
+            var result = QuadraticEquationsSolver.Solve(a, b, c);
+            foreach(var x in result )
+            {
+                Assert.AreEqual(0, a * x * x + b * x + c, 1e-10);
+            }
         }
     }
 }
